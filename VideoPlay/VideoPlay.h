4f0ff99017dd905e385ec56a3af0fccd6f02d7dc
cuitwhy <1163505981@qq.com>
2017-10-15 20:59:02 +0800

    Add:comment

--- VideoPlay/VideoPlay.h
+++ VideoPlay/VideoPlay.h
@@ -13,18 +13,18 @@ class VideoPlay : public QMainWindow
 
 public:
     VideoPlay(QWidget *parent = Q_NULLPTR);
-    void closeEvent(QCloseEvent *event);
+    void closeEvent(QCloseEvent *event);//重写窗口关闭响应函数
 
 private:
-    void LabelDisplayMat(QLabel *label, cv::Mat &mat);
+    void LabelDisplayMat(QLabel *label, cv::Mat &mat);//label显示Mat图像
     void delayMsec(int iMsec);//延时毫秒
 
 private slots:
-    void OnButtonChooseVideo();
-    void OnSpeedChange(int iSpeed);
-    void OnButtonExit();
+    void OnButtonChooseVideo();//选择视频
+    void OnSpeedChange(int iSpeed);//播放速度改变响应
+    void OnButtonExit();//退出按钮响应
 
 private:
     Ui::VideoPlayClass ui;
-    int m_iSpeed;//播放速度
+    int m_iSpeed;//播放速度，负为降速，正为提速，0为保持原速
 };
